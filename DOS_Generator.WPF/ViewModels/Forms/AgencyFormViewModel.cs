a61a6a5d893116bb979dfde6b7c2b119f95d1766
Mohammedi Abdenasser <Mdi-Nacer@users.noreply.github.com>
2020-12-23 13:52:17 +0100

    Create AgencyForm view
    a Form to Add/Edit an Agency entity

---
+++ DOS_Generator.WPF/ViewModels/Forms/AgencyFormViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System.Threading.Tasks;
+using DOS_Generator.Core;
+using DOS_Generator.Core.Models;
+
+namespace DOS_Generator.WPF.ViewModels.Forms
+{
+    public class AgencyFormViewModel : ViewModelBase
+    {
+        #region Constructors
+
+        public AgencyFormViewModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public AgencyFormViewModel()
+        {
+            
+        }
+
+        #endregion
+
+        #region Properties
+
+        public Agency Agency { get; set; } = new Agency();
+        public bool IsEdit { get; set; }
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        #endregion
+
+        #region Functions
+
+        public async Task LoadAgency(int agencyId)
+        {
+            var agency = await _unitOfWork.Agencies.GetByIdAsync(agencyId);
+            if (agency == null) return;
+            IsEdit = true;
+            Agency = agency;
+        }
+
+        public async Task<Agency> SaveAgency()
+        {
+            if (IsEdit)
+                _unitOfWork.Agencies.Update(Agency);
+            else await _unitOfWork.Agencies.AddAsync(Agency);
+            await _unitOfWork.CommitAsync();
+            return Agency;
+        }
+
+        #endregion
+
+        #region Events
+
+        #endregion
+
+        #region Commands
+
+        #endregion
+    }
+}
---
+++ DOS_Generator.WPF/Views/Forms/AgencyFormView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace DOS_Generator.WPF.Views.Forms
+{
+    /// <summary>
+    /// Interaction logic for AgencyFormView.xaml
+    /// </summary>
+    public partial class AgencyFormView
+    {
+        public AgencyFormView()
+        {
+            InitializeComponent();
+        }
+    }
+}
