4feb3a4bca53d81c1d631a86031f2e364df40d8f
Mohammedi Abdenasser <Mdi-Nacer@users.noreply.github.com>
2020-12-23 12:23:28 +0100

    Add Microsoft Hosting and Dependency Injection

--- DOS_Generator.WPF/App.xaml.cs
+++ DOS_Generator.WPF/App.xaml.cs
@@ -1,9 +1,70 @@
-﻿namespace DOS_Generator.WPF
+﻿using System;
+using System.IO;
+using System.Windows;
+using DOS_Generator.Core;
+using DOS_Generator.Data;
+using DOS_Generator.WPF.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DOS_Generator.WPF
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    ///     Interaction logic for App.xaml
     /// </summary>
     public partial class App
     {
+        private readonly IHost _host;
+
+        public App()
+        {
+            _host = Host.CreateDefaultBuilder()
+                .ConfigureAppConfiguration((context, builder) =>
+                {
+                    builder.SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", false, true);
+                }).ConfigureServices((context, services) => { ConfigureServices(context.Configuration, services); })
+                .Build();
+
+            ServiceProvider = _host.Services;
+        }
+
+        public static IServiceProvider ServiceProvider { get; private set; }
+        public static Messenger Messenger { get; private set; }
+
+        private static void ConfigureServices(IConfiguration configuration, IServiceCollection services)
+        {
+            services.AddDbContext<SecurityDbContext>(
+                options => options.UseSqlite(configuration.GetConnectionString("Default"),
+                    x => x.MigrationsAssembly("Security.Data")));
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddTransient<MainWindow>();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            if (_host != null)
+                await _host.StartAsync();
+
+            Messenger = new Messenger();
+
+            var window = ServiceProvider.GetRequiredService<MainWindow>();
+            window.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using (_host)
+            {
+                await _host.StopAsync(TimeSpan.FromSeconds(5));
+            }
+
+            base.OnExit(e);
+        }
     }
 }
