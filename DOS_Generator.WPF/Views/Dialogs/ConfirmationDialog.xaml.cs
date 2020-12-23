f4994fdd3b50312aacdd8dee0d9d83948047eafe
Mohammedi Abdenasser <Mdi-Nacer@users.noreply.github.com>
2020-12-23 13:31:48 +0100

    Added standard dialogs controls
    Confirmation dialog (YES/NO)
    Alert Dialog (For errors)

---
+++ DOS_Generator.WPF/Views/Dialogs/ConfirmationDialog.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace DOS_Generator.WPF.Views.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for ConfirmationDialog.xaml
+    /// </summary>
+    public partial class ConfirmationDialog
+    {
+        public string Message { get; set; }
+        public string Title { get; set; }
+
+        public ConfirmationDialog()
+        {
+            InitializeComponent();
+        }
+
+        public ConfirmationDialog(string message, string title)
+        {
+            Message = message;
+            Title = title;
+            InitializeComponent();
+        }
+    }
+}
---
+++ DOS_Generator.WPF/Views/Dialogs/ErrorDialog.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace DOS_Generator.WPF.Views.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for ErrorDialog.xaml
+    /// </summary>
+    public partial class ErrorDialog
+    {
+        public string Message { get; set; }
+        public string Title { get; set; }
+
+        public ErrorDialog()
+        {
+            InitializeComponent();
+        }
+
+        public ErrorDialog(string message, string title)
+        {
+            Message = message;
+            Title = title;
+            InitializeComponent();
+        }
+    }
+}
