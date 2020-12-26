3e4b53847bcf527a516977689589bb8a91832937
Mohammedi Abdenasser <Mdi-Nacer@users.noreply.github.com>
2020-12-26 10:27:51 +0100

    Removed testing email adress

--- DOS_Generator.WPF/Views/Forms/EmailCredentialsFormView.xaml.cs
+++ DOS_Generator.WPF/Views/Forms/EmailCredentialsFormView.xaml.cs
@@ -5,7 +5,7 @@ namespace DOS_Generator.WPF.Views.Forms
     public partial class EmailCredentialsFormView
     {
         public static readonly DependencyProperty EmailProperty = DependencyProperty.Register(
-            "Email", typeof(string), typeof(EmailCredentialsFormView), new PropertyMetadata("m.nacer.gn@gmail.com"));
+            "Email", typeof(string), typeof(EmailCredentialsFormView), new PropertyMetadata(default(string)));
 
         public static readonly DependencyProperty PasswordProperty = DependencyProperty.Register(
             "Password", typeof(string), typeof(EmailCredentialsFormView), new PropertyMetadata(default(string)));
